92520ec742a26a2935c1b5eaa75e58512478d96e
akashks1998 <akashks@iitk.ac.in>
2020-04-30 13:27:56 +0530

    Program v0.1 added

--- code/code.py
+++ code/code.py
@@ -3,14 +3,18 @@
 # A function is a counterexample if its block multilinear rep is not bounded
 
 from sys import argv
+import math
+import itertools
+
 #from random import seed
 #from random import randint
 
-n = 2  # number of variables
-size_tt = 2**n  # size of the truth table of f
-t = 2 # degree of the polynomials
-n_bm = t * (n + 1)
 
+n=int(input()) # number of variables
+size_tt = 2**n  # size of the truth table of f
+t=int(input()) # degree of the polynomials
+n_bm = t * (n + 1)
+permutations_t=list(itertools.permutations(list(range(t))))
 def binary(l,dig): #binary representation of l (0 replaced with -1) with dig number of digits   
     rep = []
     m=int(l)
@@ -26,16 +30,31 @@ def binary(l,dig): #binary representation of l (0 replaced with -1) with dig num
 
 #print(binary(63))
 
+
 def is_func(func):        # is this function a counterexample, needs input in block multilinear representation
     flag = 0
     for i in range(0,2**(n_bm)):
         x_b = binary(i,n_bm)  # \bar{x}
         # print(reps)
-        x_ext = [(x_b[0] * x_b[3]), (x_b[0] * x_b[4]), (x_b[1] * x_b[3]), (x_b[0] * x_b[5]),(x_b[2] * x_b[3]), (x_b[1] * x_b[5]), (x_b[2] * x_b[4])]
-        #print(x,x_ext)
+        x_ext=[]
         val = 0
-        for j in range(0,7):
-            val = val + x_ext[j] * func[j]
+        for x in range(size_tt):
+            x_ext.append([1]*math.factorial(t))
+            cnt=0
+            vars=[]
+            temp=x
+            for i in range(n):
+                if 1&temp==1:
+                    vars.append(i+1)
+                    cnt+=1
+            vars+=[0]*(t-cnt)
+
+            for y in range(math.factorial(t)):
+                for z in range(t):
+                    x_ext[x][y]*=x_b[permutations_t[y][z]*(n+1)+ vars[z]]
+                val+=func[x][y]*x_ext[x][y]
+        
+        
         #print("value= ", val)
         if ((val > 1) or (val < -1)):
             print("GOT A COUNTEREXAMPLE", "x_b= ", x_b,  "f=", func, "f(x)= ", val, "\n\n")
@@ -63,8 +82,13 @@ def poly(func_tt):   # give the block multilinear rep of f from its truth table 
             func_poly[i] = func_poly[i] + func_tt[j] * chi(i,j)
         func_poly[i] = (1.0 / size_tt) * func_poly[i]   # ith Fourier coefficient
     print("the poly rep is ", func_poly)
-    h = 0.5
-    func_bm = [func_poly[0], h * func_poly[1], h * func_poly[1], h * func_poly[2], h * func_poly[2], h * func_poly[3], h * func_poly[3]]
+    h = 1/(math.factorial(t))
+    func_bm=[]
+    for i in range(size_tt):
+        func_bm.append([0]*math.factorial(t))
+        for j in range(math.factorial(t)):
+            func_bm[i][j]=func_poly[i]*h
+    # func_bm = [func_poly[0], h * func_poly[1], h * func_poly[1], h * func_poly[2], h * func_poly[2], h * func_poly[3], h * func_poly[3]]
     return func_bm
 
 
@@ -74,7 +98,3 @@ for i in range(0,2**(size_tt)):
     f_bm = poly(f_tt)
     #print("Its bm poly representation is ", f_bm)
     is_func(f_bm)
-
-
-
-
