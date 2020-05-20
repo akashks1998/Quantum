756ec278308172d872ca2464fcfa9180b4a69aa1
akashks1998 <akashks@iitk.ac.in>
2020-05-20 12:07:26 +0530

    Code corrected

--- code/code.py
+++ code/code.py
@@ -13,13 +13,18 @@ from mip import *
 print("Give the number of variables")
 n=int(input()) # number of variables
 size_tt = 2**n  # size of the truth table of f
-print("Give the degree of the polynomials")
-t=int(input()) # degree of the polynomials
+t=0 # degree of the polynomials
 n_bm = t * (n + 1)
 count = 0 # global variable to count number of counterexamples
 
 
-permutations_t=list(itertools.permutations(list(range(t))))
+def  countSetBits(n): 
+    count = 0
+    while (n): 
+        count += n & 1
+        n >>= 1
+    return count 
+
 def binary(l,dig): #binary representation of l (0 replaced with -1) with dig number of digits   
     rep = []
     m=int(l)
@@ -38,6 +43,11 @@ def binary(l,dig): #binary representation of l (0 replaced with -1) with dig num
 
 def is_func(func):        # is this function a counterexample, needs input in block multilinear representation
     m=Model()
+    global t
+    print("t: "+str(t))
+    permutations_t=list(itertools.permutations(list(range(t))))
+    print("permutation")
+    print(permutations_t)
     x_b=[]
     x_ext=[]
     x_sum=[]
@@ -57,18 +67,20 @@ def is_func(func):        # is this function a counterexample, needs input in bl
             temp//=2
         vars+=[0]*(t-cnt)
         for y in range(t_fact):
-            m+=x_ext[x][y]+2*x_sum[x][y]==xsum(x_b[vars[z]][permutations_t[y][z]] for z in range(t))
+            m+=(x_ext[x][y]+2*x_sum[x][y]==xsum(x_b[vars[z]][permutations_t[y][z]] for z in range(t)))
 
-    m.objective=xsum( func[x][y]*(1-2*x_ext[x][y]) for x in range(size_tt) for j in range(t_fact) )
-    status = m.optimize(max_seconds=300)
+    m.objective=xsum( func[x][y]*(1-2*x_ext[x][y]) for x in range(size_tt) for y in range(t_fact) )
+    status = m.optimize(max_seconds=500)
     if status == OptimizationStatus.OPTIMAL:
         print('Optimal value is'+str(m.objective_value))
         if m.objective_value<-1-1e-5:
             print('Counter eample found with {} as its minimum value'.format(m.objective_value))
-            for v in m.vars:
+            for k in x_ext:
                 # if abs(v.x) > 1e-6: # only printing non-zeros
-                print('{} : {}'.format(v.name, v.x))
+                for v in k:
+                    print('{} : {}'.format(v.name, v.x))
             print(func)
+            exit()
             # exit()
 
 def chi(i,j):  # Calculates \chi_i(j) , where i should be viewed as a set (-1 means index not present) and j should be viewed as an assignment.
@@ -83,25 +95,32 @@ def chi(i,j):  # Calculates \chi_i(j) , where i should be viewed as a set (-1 me
 
 
 def poly(func_tt):   # give the block multilinear rep of f from its truth table representation, the first entry of truth table is -1,-1,..., -1
+    global t
+    t=0
     func_poly = [0] * size_tt
     for i in range(0,size_tt):
         func_poly[i] = 0
         for j in range(0,size_tt):
             func_poly[i] = func_poly[i] + func_tt[j] * chi(i,j)
         func_poly[i] = (1.0 / size_tt) * func_poly[i]   # ith Fourier coefficient
+        if abs(func_poly[i])>1e-6:
+            t=max(countSetBits(i),t)
     #print("the poly rep is ", func_poly)
-    h = 1/(math.factorial(t))
+    
+    t_fact=math.factorial(t)
+    h = 1/(t_fact)
     func_bm=[]
+   
     for i in range(size_tt):
-        func_bm.append([0]*math.factorial(t))
-        for j in range(math.factorial(t)):
+        func_bm.append([0]*t_fact)
+        for j in range(t_fact):
             func_bm[i][j]=func_poly[i]*h
+    print(func_bm)
     return func_bm
 
 
 for i in range(0,2**(size_tt)):
     f_tt = binary(i, size_tt)
-# f_tt=[1,-1,-1,-1]
     print("The truth table of the function is ", f_tt)
     f_bm = poly(f_tt)
     #print("Its bm poly representation is ", f_bm)
